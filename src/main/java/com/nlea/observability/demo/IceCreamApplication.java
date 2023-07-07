60d1d6ac93e601e94b84ed48e8689947c6dc0d7a
Nele Lea Uhlemann <nlea@Neles-MBP.fritz.box>
2023-07-07 15:38:36 +0200

    adding @EnableAutometrics to SoringBootApplication

--- src/main/java/com/nlea/observability/demo/IceCreamApplication.java
+++ src/main/java/com/nlea/observability/demo/IceCreamApplication.java
@@ -1,9 +1,12 @@
 package com.nlea.observability.demo;
 
+import com.autometrics.bindings.EnableAutometrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutometrics
+
 public class IceCreamApplication {
 
 	public static void main(String[] args) {
